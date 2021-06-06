47a7236113a6f2092ac55b391a43263aac35ea22
Luis Gustavo Macedo <luisgustavomacedo13@gmail.com>
2021-06-06 01:21:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -20,53 +20,63 @@ function searchWeather(searchterm){
 }
 
 function init(resultFromServer){
-        console.log(resultFromServer)
+    if(resultFromServer.cod == '404'){
+        alert('Cidade inexistente ou fora do nosso banco de dados')
+    } else {
         let city = resultFromServer.name
         document.querySelector('.cidade').innerHTML = city;
-    
+
         let temperature = (( resultFromServer.main.temp - 32) * 5/9).toFixed(2);
         document.querySelector('.temperatura').innerHTML = `Temperatura: ${ temperature}°C` 
-    
+
         let timeCondition = resultFromServer.weather[0].description;
         document.querySelector('.condiçãoDoTempo').innerHTML = timeCondition
-    
+
         let clouds = resultFromServer.clouds.all;
         document.querySelector('.nuvens').innerHTML = `Nuvens: ${ clouds}%`
-    
+
         let humidity = resultFromServer.main.humidity
         document.querySelector('.umidade').innerHTML =
         `Umidade: ${ humidity}%`
 
         let icon = resultFromServer.weather[0].icon
         document.querySelector('#img').setAttribute('src', `http://openweathermap.org/img/wn/${icon}@2x.png`)
-        
+    
         conditionalBackground(icon)
     }
+}
 
 function conditionalBackground(icon){
     switch (icon){
         case '01d':
             document.body.style.backgroundImage = "url('Foto01d.jpg')"
         break;
+
         case '01n':
             document.body.style.backgroundImage = "url('foto01n.jpg')"
         break;
-        case '02d' || '03d' || '04d':
+
+        case '02d', '03d', '04d':
             document.body.style.backgroundImage = "url('foto02d.jpg')"
         break;
-        case '02n' || '03n' || '04n':
+
+        case '02n', '03n', '04n':
             document.body.style.backgroundImage = "url('foto02n.jpg')"
         break;
-        case '05d' || '05n':
+
+        case '05d', '05n':
             document.body.style.backgroundImage = "url('foto04dn.webp')"
         break;
-        case '10d' || '10n':
+
+        case '10d', '10n':
             document.body.style.backgroundImage = "url('foto10dn.jpg')"
         break;
-        case '11d' || '11n':
+
+        case '11d', '11n':
             document.body.style.backgroundImage = "url('foto11dn.jpg')"
         break;
-        case '13d' || '13n':
+        
+        case '13d', '13n':
             document.body.style.backgroundImage = "url('foto13dn.jpg')"
         break;
     } 
